22057231cb54dd6da256ef390dfb36107695ed25
adig02 <200640837@student.georgianc.on.ca>
2025-09-09 20:01:51 -0400

    week2

--- Week-2/config.php
+++ Week-2/config.php
@@ -4,7 +4,7 @@
 // ==========================
 
 // Replace with your TMDB API key
-define("TMDB_API_KEY", "2fe46916e53df10edb0495a63d63326f");
+define("TMDB_API_KEY", "enterAPIKey");
 
 // Base URL for TMDB
 define("TMDB_BASE_URL", "https://api.themoviedb.org/3");
