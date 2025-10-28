5121ead4bfd174bc493eb6afae8c710bd5a5f056
adig02 <200640837@student.georgianc.on.ca>
2025-10-28 11:46:39 -0400

    COMP1006 - Week 8 - Updated file to contain our database connection

--- Week-8/includes/database.php
+++ Week-8/includes/database.php
@@ -1,2 +1,23 @@
 <?php
 
+class Database {
+    private $host = "172.31.22.43";
+    private $username = "Adi200640837";
+    private $password = "sW_5gvTdvJ";
+    private $db_name = "Adi200640837";
+    public $conn;
+
+    public function connect() {
+        $this->conn = null;
+        try{
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch (PDOException $e){
+            echo "<p>Connection failed: " . $e->getMessage() . "</p>";
+        }
+        return $this->conn;
+    }
+
+}
+
