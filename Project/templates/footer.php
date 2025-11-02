4ef4d200ac04096ace8f9c298cd40250a2ac24f7
adig02 <200640837@student.georgianc.on.ca>
2025-11-02 15:46:30 -0500

    Initial Header & Footer Templates

--- Project/templates/footer.php
+++ Project/templates/footer.php
@@ -1,1 +1,7 @@
-<?php
+        </main>
+        <footer>
+            <h6>&copy; <?php echo date("Y"); ?> PROJECTNAME | By Adi Gershman</h6>
+            <h6> COMP1006 - Project</h6>
+        </footer>
+    </body>
+</html>
--- Project/templates/header.php
+++ Project/templates/header.php
@@ -1,1 +1,41 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php echo "$pageTitle;" ?></title>
+    <meta name="description" content="<?php echo "$pageDesc;" ?>">
+    <meta name="robots" content="noindex,nofollow">
+    <!--  Custom Fonts  -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,600;1,400&family=Montserrat+Alternates:wght@400;500&display=swap"
+          rel="stylesheet">
+    <!--  Bootstrap  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"></script>
+    <!--  Add CSS  -->
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+    <header>
+        <!-- logo container -->
+        <div>
+            <img src="#" alt="logo">
+        </div>
+        <!-- menu bar -->
+        <nav>
+            <menu>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="#">PLACEHOLDER</a></li>
+                <li><a href="#">PLACEHOLDER</a></li>
+                <li><a href="#">PLACEHOLDER</a></li>
+            </menu>
+        </nav>
+        <!-- mobile hamburger icon -->
+        <svg id="mobile-menu" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100"
+             viewBox="0 0 50 50">
+            <path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"></path>
+        </svg>
+    </header>
+    <main>
