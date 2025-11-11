e2ff828c7f3585ef6c6268ee33104e561e94fcd1
adig02 <200640837@student.georgianc.on.ca>
2025-11-11 17:13:29 -0500

    Footer & Header CSS redesign

--- Project/templates/footer.php
+++ Project/templates/footer.php
@@ -4,6 +4,12 @@
                 <div>
                     <h6>RECLAIM</h6>
                     <p>Helping people and property find their way back through secure, verified lost and found management.</p>
+                    <div class="footer-socials">
+                        <i class="fa-brands fa-square-facebook fa-3x"></i>
+                        <i class="fa-brands fa-square-instagram fa-3x"></i>
+                        <i class="fa-brands fa-square-x-twitter fa-3x"></i>
+                        <i class="fa-brands fa-square-linkedin fa-3x"></i>
+                    </div>
                 </div>
                 <div class="footer-quick-links">
                     <h6>Quick Links</h6>
@@ -16,14 +22,29 @@
                 </div>
                 <div>
                     <h6>Contact Info</h6>
-                    <p>support@reclaim.com</p>
-                    <p>+1 (555) 123-4567</p>
-                    <p>Campus Security Office</p>
+                    <div class="footer-contact">
+                        <i class="fa-solid fa-envelope"></i>
+                        <p>support@reclaim.com</p>
+                    </div>
+                    <div class="footer-contact">
+                        <i class="fa-solid fa-phone"></i>
+                        <p>+1 (555) 123-4567</p>
+                    </div>
+                    <div class="footer-contact">
+                        <i class="fa-solid fa-location-dot"></i>
+                        <p>Campus Security Office</p>
+                    </div>
                 </div>
 
             </div>
-            <div>
-                <h6>&copy; <?php echo date("Y"); ?> RECLAIM. All Rights Reserved</h6>
+            <div class="footer-separator"></div>
+            <div class="footer-copy">
+                <p>&copy; <?php echo date("Y"); ?> Reclaim. All Rights Reserved.</p>
+                <div>
+                    <a href="#">Terms</a>
+                    <a href="#">Privacy</a>
+                    <a href="#">Cookies</a>
+                </div>
             </div>
         </footer>
     </body>
